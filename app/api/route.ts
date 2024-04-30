c99e502dd8837f27991db4157f354702df7014a6
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2024-04-30 18:26:35 -0500

    Couldnt implement thar

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,7 @@
 import { handleGetProyects } from "@/actions/proyectActions"; 
 
-export async function GET() {
-    const data = await handleGetProyects();
-    return Response.json(data);
-}
+// export async function GET() {
+//     const data = await handleGetProyects();
+//     return Response.json(data);
+// }
     
