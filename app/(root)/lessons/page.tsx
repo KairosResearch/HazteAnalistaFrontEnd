e6f244999822b3a8b23245241fdd50f83accd00d
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2024-06-28 19:26:51 -0500

    defined hardcoded data for lessons

--- app/(root)/lessons/page.tsx
+++ app/(root)/lessons/page.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
 } from "@/components/ui/card"
 import Link from 'next/link';
+import { aprendizaje } from '@/lib/data';
 
 const page = () => {
   const cards = [];
@@ -12,21 +13,21 @@ const page = () => {
   for (let i = 0; i < 10; i++) {
     cards.push(
       <Link key={i} href={`/lessons/${i}`}>
-        <Card  className='relative h-full'>
-        
-          <CardHeader className='max-h-[20vh]'>
-            <img style={{height:'20vh'}} src={'/lesson2.webp'} alt="" />
-          </CardHeader>
-          <CardContent>
-            <p className='text-sm line-clamp-1'>
-              Esta es una leccion
-            </p>
-            <span className='opacity-75 text-xs'>
-              08-2-2024
-            </span>
-          </CardContent>
-        </Card>
-      </Link>
+            <Card  className='relative h-full'>
+            
+              <CardHeader className='max-h-[20vh]'>
+                <img style={{height:'20vh'}} src={'/lesson2.webp'} alt="" />
+              </CardHeader>
+              <CardContent>
+                <p className='text-sm line-clamp-1'>
+                  {}
+                </p>
+                <span className='opacity-75 text-xs'>
+                  08-2-2024
+                </span>
+              </CardContent>
+            </Card>
+          </Link>
     );
   }
   return (
@@ -57,10 +58,37 @@ const page = () => {
     <h1 className='text-2xl px-3'>
             Nuestros modulos de aprndizaje
           </h1>
-      <section className='w-10/12 mt-10 md:w-[90%] md:grid grid-cols-3 mx-auto flex flex-col gap-6'>
+      <section className='w-10/12 mt-10 md:w-[90%] md:grid  mx-auto flex flex-col gap-6'>
       
         {
-          cards
+          aprendizaje.map((aprendizaje, i) => (
+            <div role='row' key={i}>
+                <h1 className='text-2xl'>
+                  Modulo {aprendizaje.modulo}
+                </h1>
+                <section className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+                  {
+                    aprendizaje.lessons.map((leccion, i) => (
+                      <Link key={i} href={`/lessons/${leccion.id}`}>
+                        <Card  className='relative h-full'>
+                          {/* <CardHeader className='max-h-[20vh]'>
+                            <img style={{height:'20vh'}} src={leccion.img} alt="" />
+                          </CardHeader> */}
+                          <CardContent>
+                            <p className='text-sm line-clamp-1'>
+                              {leccion.title}
+                            </p>
+                            <span className='opacity-75 text-xs'>
+                              {leccion.date}
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </Link>
+                    ))
+                  }
+                </section>
+            </div>
+        ))
         }
 
       </section>
